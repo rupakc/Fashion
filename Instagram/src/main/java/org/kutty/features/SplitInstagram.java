c67d1fff60ab5aa49694a67009b0ce7b5146d192
rupakc <rupak97.4@gmail.com>
2015-08-23 00:54:24 +0530

    Smoothing and Instagram Data Split created

---
+++ Instagram/src/main/java/org/kutty/features/SplitInstagram.java
@@ -0,0 +1,12 @@
+package org.kutty.features;
+
+/** 
+ * Given instagram training data randomly splits it into N mutually exclusive sets
+ * @author Rupak Chakraborty
+ * @for Kutty 
+ * @since 23 August, 2015
+ */ 
+
+public class SplitInstagram {
+
+}
