e0a12bdd07c1ebc3744a2f44ebde2f16b9b2f5e1
rupakc <rupak97.4@gmail.com>
2015-08-30 13:59:22 +0530

    Sentiment and Spam Classification Code Added

--- Instagram/src/main/java/org/kutty/classification/NaiveBayesSentiment.java
+++ Instagram/src/main/java/org/kutty/classification/NaiveBayesSentiment.java
@@ -7,76 +7,126 @@ import java.util.Map.Entry;
 import org.kutty.features.FeatureUtil;
 
 /** 
- * Naive Bayes Classifier for Instagram Giveaway detection 
- * 
+ * Defines the pipeline for sentiment analysis of posts from all channels
  * @author Rupak Chakraborty
- * @for Kutty 
- * @since 25 August,2015
+ * @for Kutty
+ * @since 29 August, 2015
  * 
  */ 
 
-public class NaiveBayesGiveaway { 
-	
-	public Map <String, Double> real_map;
-	public Map <String, Double> fake_map;
-	public Map <String, Double> real_tag_map;
-	public Map <String, Double> fake_tag_map; 
-	public String REAL_FILENAME = "giveaway/real_";
-	public String FAKE_FILENAME = "giveaway/fake_";
-	public String REAL_TAG_FILENAME = "giveaway/tag_real_";
-	public String FAKE_TAG_FILENAME = "giveaway/tag_fake_";
+public class NaiveBayesSentiment {
+
+	public Map <String, Double> positive_map;
+	public Map <String, Double> negative_map;
+	public Map <String, Double> neutral_map;
+	public Map <String, Double> positive_tag_map;
+	public Map <String, Double> negative_tag_map; 
+	public Map <String, Double> neutral_tag_map; 
+	public String POSITIVE_FILENAME = "/positive_";
+	public String NEGATIVE_FILENAME = "/negative_";
+	public String NEUTRAL_FILENAME = "/neutral_";
+	public String POSITIVE_TAG_FILENAME = "/tag_positive_";
+	public String NEGATIVE_TAG_FILENAME = "/tag_negative_";
+	public String NEUTRAL_TAG_FILENAME = "/tag_neutral_";
+	public String CHANNEL_NAME = "twitter";
 	public int MODEL_NUMBER = 1;
-	
+
 	/** 
-	 * public constructor to initialize the model number for loading giveaways
+	 * public constructor to initialize the model number and the channel name
 	 * @param model_number Integer containing the model number
+	 * @param channel_name String containing the channel name
+	 * 
 	 */ 
-	
-	public NaiveBayesGiveaway(int model_number) { 
-		
-		REAL_FILENAME = REAL_FILENAME + model_number + ".txt";
-		FAKE_FILENAME = FAKE_FILENAME + model_number + ".txt";
-		REAL_TAG_FILENAME = REAL_TAG_FILENAME +  model_number + ".txt";
-		FAKE_TAG_FILENAME = FAKE_TAG_FILENAME + model_number + ".txt";
+
+	public NaiveBayesSentiment(int model_number,String channel_name) { 
+
 		this.MODEL_NUMBER = model_number;
+		this.CHANNEL_NAME = channel_name.toLowerCase().trim();
+		POSITIVE_FILENAME = this.CHANNEL_NAME + this.POSITIVE_FILENAME + this.MODEL_NUMBER + ".txt";
+		NEGATIVE_FILENAME = this.CHANNEL_NAME + this.NEGATIVE_FILENAME + this.MODEL_NUMBER + ".txt";
+		NEUTRAL_FILENAME = this.CHANNEL_NAME + this.NEUTRAL_FILENAME + this.MODEL_NUMBER + ".txt"; 
 		
-		real_map = LoadModel.getTrainedModel(REAL_FILENAME);
-		fake_map = LoadModel.getTrainedModel(FAKE_FILENAME);
-		real_tag_map = LoadModel.getTrainedModel(REAL_TAG_FILENAME);
-		fake_tag_map = LoadModel.getTrainedModel(FAKE_TAG_FILENAME);
+		if (CHANNEL_NAME.equalsIgnoreCase("instagram")) { 
+			
+			POSITIVE_TAG_FILENAME = this.CHANNEL_NAME + this.POSITIVE_TAG_FILENAME + this.MODEL_NUMBER + ".txt";
+			NEGATIVE_TAG_FILENAME = this.CHANNEL_NAME + this.NEGATIVE_TAG_FILENAME + this.MODEL_NUMBER + ".txt";
+			NEUTRAL_TAG_FILENAME = this.CHANNEL_NAME + this.NEUTRAL_TAG_FILENAME + this.MODEL_NUMBER + ".txt";
+		}
+
+		positive_map = LoadModel.getTrainedModel(POSITIVE_FILENAME);
+		negative_map = LoadModel.getTrainedModel(NEGATIVE_FILENAME);
+		neutral_map = LoadModel.getTrainedModel(NEUTRAL_FILENAME); 
+		
+		if (CHANNEL_NAME.equalsIgnoreCase("instagram")) { 
+			
+			positive_tag_map = LoadModel.getTrainedModel(POSITIVE_TAG_FILENAME);
+			negative_tag_map = LoadModel.getTrainedModel(NEGATIVE_TAG_FILENAME);
+			neutral_tag_map = LoadModel.getTrainedModel(NEUTRAL_TAG_FILENAME);
+		}
 	}
-	
+
 	/** 
-	 * Given a captionText and Tagset returns the Giveaway class label
-	 * @param captionText String containing the caption text
-	 * @param tagset String containing the tagset
-	 * @return String containing the Giveaway label (i.e. real or fake)
+	 * Given a caption text and the associated tagset determines the sentiment
+	 * @param captionText String containing the captionText
+	 * @param tagset String containing the tagSet
+	 * @return String containing the sentiment label (i.e. positive, negative or neutral)
+	 * 
 	 */ 
-	
-	public String classifyGiveaway(String captionText,String tagset) { 
-		
+
+	public String classifySentimentInstagram(String captionText,String tagset) { 
+
 		String processCaption = preProcessingPipelineForContent(captionText);
 		String processTagSet = preProcessingPipelineForTag(tagset);
-		double [] caption_probability = new double[2];
-		double [] tag_probability = new double[2]; 
+		double [] caption_probability = new double[3];
+		double [] tag_probability = new double[3]; 
 		Map <String, Double> ngram_probabilty = new HashMap <String, Double>(); 
 		Entry<String, Double> max_entry; 
-			
+
 		processTagSet = FeatureUtil.getNGram(processTagSet, 1);
-		tag_probability[0] = getProbability(processTagSet, real_tag_map);
-		tag_probability[1] = getProbability(processTagSet, fake_tag_map);
+		tag_probability[0] = getProbability(processTagSet, positive_tag_map);
+		tag_probability[1] = getProbability(processTagSet, negative_tag_map);
+		tag_probability[2] = getProbability(processTagSet, neutral_tag_map);
 		
 		for (int i = 1; i <= 3; i++) { 
-			
+
 			processCaption = FeatureUtil.getNGram(processCaption, i);
-			caption_probability[0] = getProbability(processCaption, real_map)*tag_probability[0];
-			caption_probability[1] = getProbability(processCaption, fake_map)*tag_probability[1];
+			caption_probability[0] = getProbability(processCaption, positive_map)*tag_probability[0];
+			caption_probability[1] = getProbability(processCaption, negative_map)*tag_probability[1];
+			caption_probability[2] = getProbability(processCaption, neutral_map)*tag_probability[2];
 			ngram_probabilty.putAll(getClassLabelAndConfidence(caption_probability,i));
 		} 
-		
+
 		max_entry = getMaxEntry(ngram_probabilty); 
 		return max_entry.getKey();
 	} 
+	
+	/** 
+	 * Given contents from other channels classifies them into a particular sentiment
+	 * @param text String containing the content
+	 * @return String containing the sentiment label (i.e. positive, negative or neutral)
+	 */ 
+	
+	public String classifySentimentOtherChannels(String text) { 
+
+		String processText = preProcessingPipelineForContent(text);
+	
+		double [] content_probability = new double[3]; 
+		Map <String, Double> ngram_probabilty = new HashMap <String, Double>(); 
+		Entry<String, Double> max_entry; 
+
+		for (int i = 1; i <= 3; i++) { 
+
+			processText = FeatureUtil.getNGram(processText, i);
+			content_probability[0] = getProbability(processText, positive_map);
+			content_probability[1] = getProbability(processText, negative_map);
+			content_probability[2] = getProbability(processText, neutral_map);
+			ngram_probabilty.putAll(getClassLabelAndConfidence(content_probability,i));
+		} 
+
+		max_entry = getMaxEntry(ngram_probabilty); 
+		return max_entry.getKey();
+	} 
+
 	
 	/** 
 	 * Returns the map entry which has the maximum value
@@ -98,116 +148,120 @@ public class NaiveBayesGiveaway {
 
 		return maxentry;
 	}
-	
+
 	/** 
-	 * Returns a map of the most probable class label
-	 * @param a Double array containing the probabilities
-	 * @param ngram_model Integer containing the ngram model number
-	 * @return Map <String, Double> containing the class label and its probability
+	 * For a given model returns the most probable class along with model_number and ngram model
+	 * @param a  Array containing the class probabilities
+	 * @param ngram_model Integer containing the ngram language model number (i.e. 1,2,3)
+	 * @return Map <String, Double> containing the class label and its associated probability
 	 */ 
-	
-	public Map <String, Double> getClassLabelAndConfidence(double a[],int ngram_model) { 
-		
+
+	public Map<String, Double> getClassLabelAndConfidence(double a[],int ngram_model) { 
+
 		double max = Double.MIN_VALUE; 
 		int index = 0; 
 		Map <String, Double> max_pair = new HashMap <String, Double>(); 
-		
+
 		for (int i = 0; i < a.length; i++) { 
-			
+
 			if (a[i] > max) { 
-				
+
 				max = a[i];
 				index = i;
 			}
 		}
-		
+
 		if (index == 0) { 
+
+			max_pair.put("positive_" + ngram_model + "_" + this.MODEL_NUMBER, max); 
+
+		} else if (index == 1) { 
+
+			max_pair.put("negative_" + ngram_model + "_" + this.MODEL_NUMBER, max); 
 			
-			max_pair.put("real_" + ngram_model + "_" + this.MODEL_NUMBER, max); 
+		} else if (index == 2) { 
 			
-		} else { 
-			
-			max_pair.put("fake_" + ngram_model + "_" + this.MODEL_NUMBER, max);
+			max_pair.put("neutral_" + ngram_model + "_" + this.MODEL_NUMBER, max);
 		}
-		
+
 		return max_pair;
 	}
-	
+
 	/** 
 	 * Given a string cleans it up for necessary pre-processing
 	 * @param content String containing the content to be pre-processed
 	 * @return String which has been sanitized
 	 */ 
-	
+
 	public static String preProcessingPipelineForContent(String content) { 
-		
+
 		content = content.toLowerCase().trim();
 		content = FeatureUtil.cleanString(content);
 		content = FeatureUtil.removeStopWords(content);
 		content = FeatureUtil.getStemPerWord(content);
-		
+
 		return content;
 	}
-	
+
 	/** 
 	 * Given a tagset space separates it and removes the stopwords
 	 * @param tagset String containing the tagset
 	 * @param n Integer containing the value of N for Ngram
 	 * @return String containing the cleaned and separated tagset
 	 */ 
-	
+
 	public static String preProcessingPipelineForTag(String tagset) { 
-		
+
 		tagset = tagset.replace(","," ");
 		tagset = tagset.toLowerCase();
 		tagset = FeatureUtil.cleanString(tagset);
 		tagset = FeatureUtil.removeStopWords(tagset);
 		tagset = FeatureUtil.getStemPerWord(tagset);
-		
+
 		return tagset;
 	}
-	
+
 	/** 
-	 * Returns the probability of the given tagset and ngram map
-	 * @param tagset String containing the text which has to be classified
-	 * @param ngram_map Map <String, Double> containing the class label and its probability
-	 * @return Double containing the probability of a given text for a class label
+	 * Returns the probability of a given text in a given ngram language model
+	 * @param tagset String containing the text to be classified
+	 * @param ngram_map Map <String, Double> containing the ngram map
+	 * @return double containing the probability value
 	 */ 
-	
+
 	public double getProbability(String tagset,Map<String,Double> ngram_map) { 
-		
+
 		double count = 1.0;
 		double temp_count; 
-		
+
 		int index = -1;
 		String ngram;
 		int previous_position = 0; 
-		
+
 		index = tagset.indexOf('|');
-		
+
 		while(index != -1) { 
-			
+
 			ngram = tagset.substring(previous_position, index);
 			ngram = ngram.trim();
 			ngram = getTransformedString(ngram); 
-			
+
 			if (ngram_map.containsKey(ngram) && ngram_map.get(ngram) <= 1.0) {  
-				
+
 				temp_count = ngram_map.get(ngram); 
-				
+
 			} else { 
-				
+
 				temp_count = 0.01;
 			}
-			
+
 			count = count * temp_count;
 			previous_position = index+1;
 			index = tagset.indexOf('|',previous_position);
 		} 
-		
+
 		return count;
 	}
-	
+
 	/** 
 	 * Given an ngram encloses it with a pair of braces
 	 * @param ngram String containing the NGram
@@ -221,18 +275,18 @@ public class NaiveBayesGiveaway {
 
 		return temp;
 	}
-	
+
 	/** 
-	 * Main function to test the functionality of the class
+	 * Main function to test the functionality of the given class
 	 * @param args
 	 */ 
-	
+
 	public static void main(String args[]) { 
-		
+
 		String captionText = ". Assalamualaikum semua.. ??????. . Alhamdulillah Lagi customer yang membeli iPhone dengan admin di kedai . ????????????. . Admin bukan Hanye menjual Iphone Sahaja tau. Semua model phone admin jual. Asus,Lenovo ,Nokia ,n etc. Yang mane nak tanye harga Boleh whatsapp admin or kita bincang di kedai admin ea. ?????????????. . TRADE IN ADMIN TERIMA !!. TRADE IN ADMIN TERIMA !!. . #buyerjusttekan yang beli dengan Harga #gempakdohhsale . ??????. . Anda bila lagi?? Heeeee . original iphone. -ready stock. -seal box. -prefer cod. -postage available. . Price list :. iPhone 4 16gb - rm 630 32gb - rm 660  iPhone 4s 16gb - rm 740 32gb - rm 780 64gb - rm 800  iPhone 5 16gb - rm 1070 32gb - rm 1100 64gb - rm 1150  iPhone 5s ( space grey ) 16gb - rm 1450 32gb - rm 1500 64gb - rm 1550  iPhone 5s ( silver ) 16gb - rm 1550 32gb - rm 1600 64gb - rm 1650  iPhone 5s ( gold ) 16gb - rm 1650 32gb - rm 1700 64gb - rm 1750  iPhone 6 16gb - rm 2450 64gb - rm 2650 128gb - rm 2750  iPhone 6 plus  16gb - rm 2950 64gb - rm 3150 128gb - rm 3450 . 017-7515417. Taufik  #iPhone #iphonemurah #iphonemurahmalaysia #iphoneoriginal #iphoneoriginalmalaysia #iphonesale #saleiphone #sale #repost #contest #freegift #giweaway #repostmalaysia #giveawaymalaysia #luckydraw #batupahat #johor #cod";
 		String tagSet = "gift,giveawaymalaysia,pandorainspired,igshop,giveaway,katespademalaysia,norhanisabdaziz1stgiveaway,bazaarpaknil,hermesmalaysia,tagandwin,raya2015,giveawaycontest,readystock,igshopmalaysia,giveawaytime,repost";
-		
-		NaiveBayesGiveaway nb = new NaiveBayesGiveaway(5);
-		System.out.println(nb.classifyGiveaway(captionText, tagSet));
+
+
 	}
+
 }
--- Instagram/src/main/java/org/kutty/classification/NaiveBayesSpam.java
+++ Instagram/src/main/java/org/kutty/classification/NaiveBayesSpam.java
@@ -7,77 +7,114 @@ import java.util.Map.Entry;
 import org.kutty.features.FeatureUtil;
 
 /** 
- * Naive Bayes Classifier for Instagram Giveaway detection 
- * 
+ * Defines the class for spam detection across all channels
  * @author Rupak Chakraborty
- * @for Kutty 
- * @since 25 August,2015
+ * @for Kutty
+ * @since 29 August, 2015
  * 
- */ 
+ */
 
-public class NaiveBayesGiveaway { 
-	
-	public Map <String, Double> real_map;
-	public Map <String, Double> fake_map;
-	public Map <String, Double> real_tag_map;
-	public Map <String, Double> fake_tag_map; 
-	public String REAL_FILENAME = "giveaway/real_";
-	public String FAKE_FILENAME = "giveaway/fake_";
-	public String REAL_TAG_FILENAME = "giveaway/tag_real_";
-	public String FAKE_TAG_FILENAME = "giveaway/tag_fake_";
+public class NaiveBayesSpam {
+
+	public Map <String, Double> spam_map;
+	public Map <String, Double> ham_map;
+	public Map <String, Double> spam_tag_map;
+	public Map <String, Double> ham_tag_map; 
+	public String SPAM_FILENAME = "/spam_";
+	public String HAM_FILENAME = "/ham_";
+	public String SPAM_TAG_FILENAME = "/tag_spam_";
+	public String HAM_TAG_FILENAME = "/tag_ham_";
+	public String CHANNEL_NAME = "";
 	public int MODEL_NUMBER = 1;
-	
+
 	/** 
-	 * public constructor to initialize the model number for loading giveaways
+	 * public constructor to initialize the model number and channel name for spam detection
 	 * @param model_number Integer containing the model number
+	 * @param channel_name String containing the channel name
 	 */ 
-	
-	public NaiveBayesGiveaway(int model_number) { 
-		
-		REAL_FILENAME = REAL_FILENAME + model_number + ".txt";
-		FAKE_FILENAME = FAKE_FILENAME + model_number + ".txt";
-		REAL_TAG_FILENAME = REAL_TAG_FILENAME +  model_number + ".txt";
-		FAKE_TAG_FILENAME = FAKE_TAG_FILENAME + model_number + ".txt";
+
+	public NaiveBayesSpam(int model_number,String channel_name) { 
+
 		this.MODEL_NUMBER = model_number;
+		this.CHANNEL_NAME = channel_name.toLowerCase().trim();
+		SPAM_FILENAME = this.CHANNEL_NAME + this.SPAM_FILENAME + this.MODEL_NUMBER + ".txt";
+		HAM_FILENAME = this.CHANNEL_NAME + this.HAM_FILENAME + this.MODEL_NUMBER + ".txt"; 
 		
-		real_map = LoadModel.getTrainedModel(REAL_FILENAME);
-		fake_map = LoadModel.getTrainedModel(FAKE_FILENAME);
-		real_tag_map = LoadModel.getTrainedModel(REAL_TAG_FILENAME);
-		fake_tag_map = LoadModel.getTrainedModel(FAKE_TAG_FILENAME);
+		if (CHANNEL_NAME.equalsIgnoreCase("instagram")) { 
+			
+			SPAM_TAG_FILENAME = this.CHANNEL_NAME + this.SPAM_TAG_FILENAME + this.MODEL_NUMBER + ".txt";
+			HAM_TAG_FILENAME = this.CHANNEL_NAME + this.HAM_TAG_FILENAME + this.MODEL_NUMBER + ".txt";
+		} 
+		
+		spam_map = LoadModel.getTrainedModel(SPAM_FILENAME);
+		ham_map = LoadModel.getTrainedModel(HAM_FILENAME); 
+		
+		if (CHANNEL_NAME.equalsIgnoreCase("instagram")) { 
+			
+			spam_tag_map = LoadModel.getTrainedModel(SPAM_TAG_FILENAME);
+			ham_tag_map = LoadModel.getTrainedModel(HAM_TAG_FILENAME);
+		}
 	}
-	
+
 	/** 
-	 * Given a captionText and Tagset returns the Giveaway class label
-	 * @param captionText String containing the caption text
-	 * @param tagset String containing the tagset
-	 * @return String containing the Giveaway label (i.e. real or fake)
+	 * Given a Instagram post classifies it as spam/ham
+	 * @param captionText String containing the captionText to be classified
+	 * @param tagset String containing the tagset to be classified
+	 * @return String containing the spam label
 	 */ 
-	
-	public String classifyGiveaway(String captionText,String tagset) { 
-		
+
+	public String classifySpamInstagram(String captionText,String tagset) { 
+
 		String processCaption = preProcessingPipelineForContent(captionText);
 		String processTagSet = preProcessingPipelineForTag(tagset);
 		double [] caption_probability = new double[2];
 		double [] tag_probability = new double[2]; 
 		Map <String, Double> ngram_probabilty = new HashMap <String, Double>(); 
 		Entry<String, Double> max_entry; 
-			
+
 		processTagSet = FeatureUtil.getNGram(processTagSet, 1);
-		tag_probability[0] = getProbability(processTagSet, real_tag_map);
-		tag_probability[1] = getProbability(processTagSet, fake_tag_map);
-		
+		tag_probability[0] = getProbability(processTagSet, spam_tag_map);
+		tag_probability[1] = getProbability(processTagSet, ham_tag_map);
+
 		for (int i = 1; i <= 3; i++) { 
-			
+
 			processCaption = FeatureUtil.getNGram(processCaption, i);
-			caption_probability[0] = getProbability(processCaption, real_map)*tag_probability[0];
-			caption_probability[1] = getProbability(processCaption, fake_map)*tag_probability[1];
+			caption_probability[0] = getProbability(processCaption, spam_map)*tag_probability[0];
+			caption_probability[1] = getProbability(processCaption, ham_map)*tag_probability[1];
 			ngram_probabilty.putAll(getClassLabelAndConfidence(caption_probability,i));
 		} 
-		
+
 		max_entry = getMaxEntry(ngram_probabilty); 
+		
 		return max_entry.getKey();
 	} 
 	
+	/** 
+	 * Given a text from channels other than Instagram determines their spamicity
+	 * @param text String containing the content which is to be analyzed
+	 * @return String containing the class label (spam or ham)
+	 */ 
+	
+	public String classifySpamOtherChannels(String text) { 
+
+		String processText = preProcessingPipelineForContent(text);
+		double [] content_probability = new double[2];
+		Map <String, Double> ngram_probabilty = new HashMap <String, Double>(); 
+		Entry<String, Double> max_entry; 
+
+		for (int i = 1; i <= 3; i++) { 
+
+			processText = FeatureUtil.getNGram(processText, i);
+			content_probability[0] = getProbability(processText, spam_map);
+			content_probability[1] = getProbability(processText, ham_map);
+			ngram_probabilty.putAll(getClassLabelAndConfidence(content_probability,i));
+		} 
+
+		max_entry = getMaxEntry(ngram_probabilty); 
+		
+		return max_entry.getKey();
+	} 
+
 	/** 
 	 * Returns the map entry which has the maximum value
 	 * @param ngram_output HashMap<String,Double> containing the value of the scores and labels for each class
@@ -98,116 +135,116 @@ public class NaiveBayesGiveaway {
 
 		return maxentry;
 	}
-	
+
 	/** 
-	 * Returns a map of the most probable class label
-	 * @param a Double array containing the probabilities
+	 * Given the probabilities of being in a given class returns the most plausible class label
+	 * @param a Double array containing the probabilities of the class labels
 	 * @param ngram_model Integer containing the ngram model number
-	 * @return Map <String, Double> containing the class label and its probability
+	 * @return Map <String, Double> containing the class label appended with ngram_model and model number
 	 */ 
-	
-	public Map <String, Double> getClassLabelAndConfidence(double a[],int ngram_model) { 
-		
+
+	public Map<String, Double> getClassLabelAndConfidence(double a[],int ngram_model) { 
+
 		double max = Double.MIN_VALUE; 
 		int index = 0; 
 		Map <String, Double> max_pair = new HashMap <String, Double>(); 
-		
+
 		for (int i = 0; i < a.length; i++) { 
-			
+
 			if (a[i] > max) { 
-				
+
 				max = a[i];
 				index = i;
 			}
 		}
-		
+
 		if (index == 0) { 
-			
-			max_pair.put("real_" + ngram_model + "_" + this.MODEL_NUMBER, max); 
-			
+
+			max_pair.put("spam_" + ngram_model + "_" + this.MODEL_NUMBER, max); 
+
 		} else { 
-			
-			max_pair.put("fake_" + ngram_model + "_" + this.MODEL_NUMBER, max);
+
+			max_pair.put("ham_" + ngram_model + "_" + this.MODEL_NUMBER, max);
 		}
-		
+
 		return max_pair;
 	}
-	
+
 	/** 
 	 * Given a string cleans it up for necessary pre-processing
 	 * @param content String containing the content to be pre-processed
 	 * @return String which has been sanitized
 	 */ 
-	
+
 	public static String preProcessingPipelineForContent(String content) { 
-		
+
 		content = content.toLowerCase().trim();
 		content = FeatureUtil.cleanString(content);
 		content = FeatureUtil.removeStopWords(content);
 		content = FeatureUtil.getStemPerWord(content);
-		
+
 		return content;
 	}
-	
+
 	/** 
 	 * Given a tagset space separates it and removes the stopwords
 	 * @param tagset String containing the tagset
 	 * @param n Integer containing the value of N for Ngram
 	 * @return String containing the cleaned and separated tagset
 	 */ 
-	
+
 	public static String preProcessingPipelineForTag(String tagset) { 
-		
+
 		tagset = tagset.replace(","," ");
 		tagset = tagset.toLowerCase();
 		tagset = FeatureUtil.cleanString(tagset);
 		tagset = FeatureUtil.removeStopWords(tagset);
 		tagset = FeatureUtil.getStemPerWord(tagset);
-		
+
 		return tagset;
 	}
-	
+
 	/** 
-	 * Returns the probability of the given tagset and ngram map
-	 * @param tagset String containing the text which has to be classified
+	 * Returns the probability of a given label for a given class
+	 * @param tagset String containing the text which is to be evaluated
 	 * @param ngram_map Map <String, Double> containing the class label and its probability
-	 * @return Double containing the probability of a given text for a class label
+	 * @return Double containing the probability of the class label
 	 */ 
-	
+
 	public double getProbability(String tagset,Map<String,Double> ngram_map) { 
-		
+
 		double count = 1.0;
 		double temp_count; 
-		
+
 		int index = -1;
 		String ngram;
 		int previous_position = 0; 
-		
+
 		index = tagset.indexOf('|');
-		
+
 		while(index != -1) { 
-			
+
 			ngram = tagset.substring(previous_position, index);
 			ngram = ngram.trim();
 			ngram = getTransformedString(ngram); 
-			
+
 			if (ngram_map.containsKey(ngram) && ngram_map.get(ngram) <= 1.0) {  
-				
+
 				temp_count = ngram_map.get(ngram); 
-				
+
 			} else { 
-				
+
 				temp_count = 0.01;
 			}
-			
+
 			count = count * temp_count;
 			previous_position = index+1;
 			index = tagset.indexOf('|',previous_position);
 		} 
-		
+
 		return count;
 	}
-	
+
 	/** 
 	 * Given an ngram encloses it with a pair of braces
 	 * @param ngram String containing the NGram
@@ -221,18 +258,17 @@ public class NaiveBayesGiveaway {
 
 		return temp;
 	}
-	
+
 	/** 
-	 * Main function to test the functionality of the class
+	 * Main function to test the functionality of the given class
 	 * @param args
 	 */ 
-	
+
 	public static void main(String args[]) { 
-		
+
 		String captionText = ". Assalamualaikum semua.. ??????. . Alhamdulillah Lagi customer yang membeli iPhone dengan admin di kedai . ????????????. . Admin bukan Hanye menjual Iphone Sahaja tau. Semua model phone admin jual. Asus,Lenovo ,Nokia ,n etc. Yang mane nak tanye harga Boleh whatsapp admin or kita bincang di kedai admin ea. ?????????????. . TRADE IN ADMIN TERIMA !!. TRADE IN ADMIN TERIMA !!. . #buyerjusttekan yang beli dengan Harga #gempakdohhsale . ??????. . Anda bila lagi?? Heeeee . original iphone. -ready stock. -seal box. -prefer cod. -postage available. . Price list :. iPhone 4 16gb - rm 630 32gb - rm 660  iPhone 4s 16gb - rm 740 32gb - rm 780 64gb - rm 800  iPhone 5 16gb - rm 1070 32gb - rm 1100 64gb - rm 1150  iPhone 5s ( space grey ) 16gb - rm 1450 32gb - rm 1500 64gb - rm 1550  iPhone 5s ( silver ) 16gb - rm 1550 32gb - rm 1600 64gb - rm 1650  iPhone 5s ( gold ) 16gb - rm 1650 32gb - rm 1700 64gb - rm 1750  iPhone 6 16gb - rm 2450 64gb - rm 2650 128gb - rm 2750  iPhone 6 plus  16gb - rm 2950 64gb - rm 3150 128gb - rm 3450 . 017-7515417. Taufik  #iPhone #iphonemurah #iphonemurahmalaysia #iphoneoriginal #iphoneoriginalmalaysia #iphonesale #saleiphone #sale #repost #contest #freegift #giweaway #repostmalaysia #giveawaymalaysia #luckydraw #batupahat #johor #cod";
 		String tagSet = "gift,giveawaymalaysia,pandorainspired,igshop,giveaway,katespademalaysia,norhanisabdaziz1stgiveaway,bazaarpaknil,hermesmalaysia,tagandwin,raya2015,giveawaycontest,readystock,igshopmalaysia,giveawaytime,repost";
-		
-		NaiveBayesGiveaway nb = new NaiveBayesGiveaway(5);
-		System.out.println(nb.classifyGiveaway(captionText, tagSet));
+
 	}
+
 }
