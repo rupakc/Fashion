f902a2acf9a5cc2baf2323d18d33f0fc1ff8f391
rupakc <rupak97.4@gmail.com>
2015-09-16 12:11:38 +0530

    Stub for Satisfaction Analytics

--- Instagram/src/main/java/org/kutty/dbo/Sentiment.java
+++ Instagram/src/main/java/org/kutty/dbo/Sentiment.java
@@ -14,6 +14,8 @@ public class Sentiment {
 	private String content;
 	private Double sentimentScore;
 	private Double otherDate; 
+	private int modelNumber;
+	private int ngramNumber; 
 	
 	/**
 	 * @return
@@ -23,88 +25,212 @@ public class Sentiment {
 		return product;
 	} 
 	
+	/** 
+	 * 
+	 * @param product
+	 */ 
+	
 	public void setProduct(String product) { 
 		
 		this.product = product;
 	}
+	
+	/** 
+	 * 
+	 * @return
+	 */ 
 	
 	public HashMap<String, Double> getSentimentDistribution() { 
 		
 		return sentimentDistribution;
 	}
 	
+	/** 
+	 * 
+	 * @param sentimentDistribution
+	 */ 
+	
 	public void setSentimentDistribution(HashMap<String, Double> sentimentDistribution) { 
 		
 		this.sentimentDistribution = sentimentDistribution;
 	}
+	
+	/** 
+	 * 
+	 * @return
+	 */ 
 	
 	public String getAuthor() {
 		
 		return author;
 	}
 	
+	/** 
+	 * 
+	 * @param author
+	 */ 
+	
 	public void setAuthor(String author) {
 		
 		this.author = author;
 	}
+	
+	/** 
+	 * 
+	 * @return
+	 */ 
 	
 	public Date getTimestamp() { 
 		
 		return timestamp;
 	}
 	
+	/** 
+	 * 
+	 * @param timestamp
+	 */ 
+	
 	public void setTimestamp(Date timestamp) { 
 		
 		this.timestamp = timestamp;
 	}
+	
+	/** 
+	 * 
+	 * @return
+	 */ 
 	
 	public String getChannel() { 
 		
 		return channel;
 	}
 	
+	/** 
+	 * 
+	 * @param channel
+	 */ 
+	
 	public void setChannel(String channel) { 
 		
 		this.channel = channel;
 	}
+	
+	/** 
+	 * 
+	 * @return
+	 */ 
 	
 	public String getSentimentLabel() { 
 		
 		return sentimentLabel;
 	}
 	
+	/** 
+	 * 
+	 * @param sentimentLabel
+	 */ 
+	
 	public void setSentimentLabel(String sentimentLabel) {  
 		
 		this.sentimentLabel = sentimentLabel;
 	}
+	
+	/** 
+	 * 
+	 * @return
+	 */ 
 	
 	public String getContent() { 
 		
 		return content;
 	}
 	
+	/** 
+	 * 
+	 * @param content
+	 */ 
+	
 	public void setContent(String content) { 
 		
 		this.content = content;
 	}
+	
+	/** 
+	 * 
+	 * @return
+	 */ 
 	
 	public Double getSentimentScore() { 
 		
 		return sentimentScore;
 	}
 	
+	/** 
+	 * 
+	 * @param sentimentScore
+	 */ 
+	
 	public void setSentimentScore(Double sentimentScore) { 
 		
 		this.sentimentScore = sentimentScore;
 	}
+	
+	/** 
+	 * 
+	 * @return
+	 */ 
 	
 	public Double getOtherDate() { 
 		
 		return otherDate;
 	}
 	
+	/** 
+	 * 
+	 * @param otherDate
+	 */
+	
 	public void setOtherDate(Double otherDate) { 
 		
 		this.otherDate = otherDate;
+	}
+
+	/**
+	 * @return the modelNumber
+	 */ 
+	
+	public int getModelNumber() { 
+		
+		return modelNumber;
+	}
+
+	/** 
+	 * 
+	 * @param modelNumber the modelNumber to set
+	 */ 
+	
+	public void setModelNumber(int modelNumber) { 
+		
+		this.modelNumber = modelNumber;
+	}
+
+	/** 
+	 * 
+	 * @return the ngramNumber
+	 */ 
+	
+	public int getNgramNumber() { 
+		
+		return ngramNumber;
+	}
+
+	/** 
+	 * 
+	 * @param ngramNumber the ngramNumber to set
+	 */ 
+	
+	public void setNgramNumber(int ngramNumber) { 
+		
+		this.ngramNumber = ngramNumber;
 	}
 }
---
+++ Instagram/src/main/java/org/kutty/satisfaction/SatisfactionIndex.java
@@ -0,0 +1,12 @@
+package org.kutty.satisfaction;
+
+/** 
+ * Calculates the Satisfaction Index of a given product and stores it in the database
+ * @author Rupak Chakraborty
+ * @for Kutty
+ * @since 15 September, 2015
+ */ 
+
+public class SatisfactionIndex {
+
+}
