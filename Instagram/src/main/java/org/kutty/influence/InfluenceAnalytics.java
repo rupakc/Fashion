4556b9e80e863b796e9589c247f7c5a0738c2daf
rupakc <rupak97.4@gmail.com>
2015-09-23 23:04:12 +0530

    Stub for Influence and Satisfaction Analytics

---
+++ Instagram/src/main/java/org/kutty/influence/InfluenceAnalytics.java
@@ -0,0 +1,14 @@
+package org.kutty.influence;
+
+/** 
+ * Carries out analytics on the influence index calculated 
+ * 
+ * @author Rupak Chakraborty
+ * @for Kutty
+ * @since 23 September,2015
+ *
+ */ 
+
+public class InfluenceAnalytics {
+
+}
---
+++ Instagram/src/main/java/org/kutty/satisfaction/SatisfactionAnalytics.java
@@ -0,0 +1,13 @@
+package org.kutty.satisfaction;
+
+/** 
+ * Performs satisfaction analytics on the satisfaction collection 
+ * 
+ * @author Rupak Chakraborty
+ * @for Kutty
+ * @since 23 September,2015
+ */
+
+public class SatisfactionAnalytics {
+
+}
